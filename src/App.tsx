f84eca00df9b04610a9fc5ea57e5fdc5bec325b4
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-24 22:47:09 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PersonCards } from '@/components';
+
+function App() {
+  return (
+    <main className='min-h-screen grid place-items-center bg-lightGrayishBlue'>
+      <PersonCards />
+    </main>
+  );
+}
+
+export default App;
