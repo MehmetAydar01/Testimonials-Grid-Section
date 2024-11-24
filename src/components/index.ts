e947606cf9376e1edf072c38d39dc04063c6bd22
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-25 02:14:25 +0300

    card komponentleri export edildi

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as PersonCards } from './PersonCards';
+export { default as FirstPersonCard } from './FirstPersonCard';
+export { default as SecondPersonCard } from './SecondPersonCard';
+export { default as ThirdPersonCard } from './ThirdPersonCard';
+export { default as FourthPersonCard } from './FourthPersonCard';
+export { default as FifthPersonCard } from './FifthPersonCard';
